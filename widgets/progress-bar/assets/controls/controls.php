cead7c7b8c48f8cc3b297460be523103d8d02eff
Sunil Kumar <sunilkumar@Sunils-Laptop.local>
2025-09-18 11:22:29 +0530

    Progress Bar: Work in progress

--- widgets/progress-bar/assets/controls/controls.php
+++ widgets/progress-bar/assets/controls/controls.php
@@ -17,109 +17,133 @@ use \Elementor\Group_Control_Background;
 use \Elementor\Group_Control_Border;
 
 // Content tab.
-// image controls.
-$this->start_controls_section(
-	'configuration',
-	array(
+$this->start_controls_section( 
+'configuration_section',
+	array( 
 		'label' => esc_html__( 'Configuration', 'wpmozo-addons-lite-for-elementor' ),
 		'tab'   => Controls_Manager::TAB_CONTENT,
 	)
 );
-$this->add_control(
-	'split_image',
-	array(
-		'label'       => esc_html__( 'Choose Image', 'wpmozo-addons-lite-for-elementor' ),
-		'type'        => Controls_Manager::MEDIA,
-		'dynamic'     => array(
-			'active' => true,
-		),
-		'default'     => array(
-			'url' => Utils::get_placeholder_image_src(),
-			'id'  => '0',
-		),
-		'render_type' => 'template',
-	)
-);
-$this->add_responsive_control(
-	'image_row',
-	array(
-		'label'       => esc_html__( 'Rows', 'wpmozo-addons-lite-for-elementor' ),
-		'type'        => Controls_Manager::SLIDER,
-		'range'       => array(
-			'px' => array(
-				'min'  => 1,
-				'max'  => 20,
-				'step' => 1,
-			),
-		),
-		'default'     => array(
-			'size' => 3,
+$this->add_control( 
+	'layout',
+	array( 
+		'label'       => esc_html__( 'Layout', 'wpmozo-addons-lite-for-elementor' ),
+		'type'        => Controls_Manager::SELECT,
+		'default'     => 'bar',
+		'options'     => array( 
+			'bar' 			=> esc_html__( 'Bar', 'wpmozo-addons-lite-for-elementor' ),
+			'circle' 		=> esc_html__( 'Circle', 'wpmozo-addons-lite-for-elementor' ),
+			'half_circle' 	=> esc_html__( 'Half Circle', 'wpmozo-addons-lite-for-elementor' ),
 		),
 		'render_type' => 'template',
 	)
 );
-$this->add_responsive_control(
-	'image_columns',
-	array(
-		'label'       => esc_html__( 'Columns', 'wpmozo-addons-lite-for-elementor' ),
-		'type'        => Controls_Manager::SLIDER,
-		'range'       => array(
-			'px' => array(
-				'min'  => 1,
-				'max'  => 20,
-				'step' => 1,
+$this->add_control( 
+	'bar_direction',
+	array( 
+		'label'       => esc_html__( 'Bar Direction', 'wpmozo-addons-lite-for-elementor' ),
+		'type'        => Controls_Manager::SELECT,
+		'default'     => 'horizontal',
+		'options'     => array( 
+			'horizontal' 	=> esc_html__( 'Horizontal', 'wpmozo-addons-lite-for-elementor' ),
+			'vertical' 		=> esc_html__( 'Vertical', 'wpmozo-addons-lite-for-elementor' ),
 			),
-		),
-		'default'     => array(
-			'size' => 3,
-		),
 		'render_type' => 'template',
+		'condition'    => array( 
+			'layout' => 'bar',
+		),
 	)
 );
-$this->add_control(
-	'image_gap',
-	array(
-		'label'       => esc_html__( 'Gap', 'wpmozo-addons-lite-for-elementor' ),
-		'type'        => Controls_Manager::SLIDER,
-		'range'       => array(
-			'px' => array(
-				'min'  => 0,
-				'max'  => 100,
-				'step' => 1,
-			),
+$this->add_control( 
+	'show_striped',
+	array( 
+		'label'        => esc_html__( 'Show Striped', 'wpmozo-addons-lite-for-elementor' ),
+		'type'         => Controls_Manager::SWITCHER,
+		'label_on'     => esc_html__( 'Yes', 'wpmozo-addons-lite-for-elementor' ),
+		'label_off'    => esc_html__( 'No', 'wpmozo-addons-lite-for-elementor' ),
+		'return_value' => 'yes',
+		'default'      => 'no',
+		'condition'    => array( 
+			'layout' => 'bar',
 		),
-		'default'     => array(
-			'size' => 15,
-			'unit' => 'px',
-		),
-		'render_type' => 'template',
+	)
+);
+$this->add_control( 
+	'show_progress_number',
+	array( 
+		'label'        => esc_html__( 'Show Progress Number', 'wpmozo-addons-lite-for-elementor' ),
+		'type'         => Controls_Manager::SWITCHER,
+		'label_on'     => esc_html__( 'Yes', 'wpmozo-addons-lite-for-elementor' ),
+		'label_off'    => esc_html__( 'No', 'wpmozo-addons-lite-for-elementor' ),
+		'return_value' => 'yes',
+		'default'      => 'yes',
 	)
 );
 $this->end_controls_section();
 
-
 // Style tab controls.
 $this->start_controls_section(
-	'split_portion_style',
+	'bar_styling',
 	array(
-		'label' => esc_html__( 'Split Portion', 'wpmozo-addons-lite-for-elementor' ),
+		'label' => esc_html__( 'Bar Styling', 'wpmozo-addons-lite-for-elementor' ),
 		'tab'   => Controls_Manager::TAB_STYLE,
 	)
 );
+$this->add_responsive_control( 
+	'bar_size',
+	array( 
+		'label'     => esc_html__( 'Bar Size', 'wpmozo-addons-lite-for-elementor' ),
+		'type'      => Controls_Manager::SLIDER,
+		'range'     => array( 
+			'px' => array( 
+				'min'  => 5,
+				'max'  => 150,
+				'step' => 1,
+			 ),
+		),
+		'default'   => array( 
+			'unit' => 'px',
+			'size' => 30,
+		),
+		'selectors' => array( 
+			'{{WRAPPER}} .dipl_progress_bar_layout_bar' => 'height: {{SIZE}}{{UNIT}};',
+		),
+	 )
+ );
 $this->start_controls_tabs(
-	'portion_normal_and_hover_tabs'
+	'bar_normal_and_hover_tabs'
 );
 $this->start_controls_tab(
-	'portion_normal_tab',
+	'bar_normal_tab',
 	array(
 		'label' => esc_html__( 'Normal', 'wpmozo-addons-lite-for-elementor' ),
+	)
+);
+$this->add_control( 
+	'bar_empty_color',
+	array( 
+		'label'     => esc_html__( 'Bar Empty Color', 'wpmozo-addons-lite-for-elementor' ),
+		'type'      => Controls_Manager::COLOR,
+		'selectors' => array( 
+			'{{WRAPPER}} .dipl_progress_bar_wrapper' => 'background-color: {{VALUE}};',
+		),
+	)
+);
+$this->add_control( 
+	'bar_filled_color',
+	array( 
+		'label'     => esc_html__( 'Bar Filled Color', 'wpmozo-addons-lite-for-elementor' ),
+		'type'      => Controls_Manager::COLOR,
+		'selectors' => array( 
+			'{{WRAPPER}} .dipl_progress_bar_layout_bar .dipl_progress_bar_inner' => 'background-color: {{VALUE}};',
+		),
 	)
 );
 $this->add_group_control(
 	Group_Control_Border::get_type(),
 	array(
-		'name'           => 'portion_border',
-		'selector'       => '{{WRAPPER}} .wpmozo_split_image .wpmozo_split_image_portion',
+		'name'           => 'bar_border',
+		'selector'       => '{{WRAPPER}} .dipl_progress_bar_layout_bar',
 		'fields_options' => array(
 			'border' => array( 'label' => esc_html__( 'Border Type', 'wpmozo-addons-lite-for-elementor' ) ),
 			'width'  => array( 'label' => esc_html__( 'Border Width', 'wpmozo-addons-lite-for-elementor' ) ),
@@ -128,30 +152,49 @@ $this->add_group_control(
 	)
 );
 $this->add_responsive_control(
-	'portion_border_radius',
+	'bar_border_radius',
 	array(
 		'label'       => esc_html__( 'Border Radius', 'wpmozo-addons-lite-for-elementor' ),
 		'type'        => Controls_Manager::DIMENSIONS,
 		'label_block' => true,
-		'separator'   => 'after',
 		'size_units'  => array( 'px', 'em', '%' ),
 		'selectors'   => array(
-			'{{WRAPPER}} .wpmozo_split_image .wpmozo_split_image_portion' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};transition:all 300ms;',
+			'{{WRAPPER}} .dipl_progress_bar_layout_bar' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};transition:all 300ms;',
 		),
 	)
 );
 $this->end_controls_tab();
 $this->start_controls_tab(
-	'portion_hover_tab',
+	'bar_hover_tab',
 	array(
 		'label' => esc_html__( 'Hover', 'wpmozo-addons-lite-for-elementor' ),
+	)
+);
+$this->add_control( 
+	'bar_empty_color_hover',
+	array( 
+		'label'     => esc_html__( 'Bar Empty Color', 'wpmozo-addons-lite-for-elementor' ),
+		'type'      => Controls_Manager::COLOR,
+		'selectors' => array( 
+			'{{WRAPPER}} .dipl_progress_bar_wrapper:hover' => 'background-color: {{VALUE}};',
+		),
+	)
+);
+ $this->add_control( 
+	'bar_filled_color_hover',
+	array( 
+		'label'     => esc_html__( 'Bar Filled Color', 'wpmozo-addons-lite-for-elementor' ),
+		'type'      => Controls_Manager::COLOR,
+		'selectors' => array( 
+			'{{WRAPPER}} .dipl_progress_bar_layout_bar .dipl_progress_bar_inner:hover' => 'background-color: {{VALUE}};',
+		),
 	)
 );
 $this->add_group_control(
 	Group_Control_Border::get_type(),
 	array(
-		'name'           => 'portion_border_hover',
-		'selector'       => '{{WRAPPER}} .wpmozo_split_image .wpmozo_split_image_portion:hover',
+		'name'           => 'bar_border_hover',
+		'selector'       => '{{WRAPPER}} .dipl_progress_bar_layout_bar:hover',
 		'fields_options' => array(
 			'border' => array( 'label' => esc_html__( 'Border Type', 'wpmozo-addons-lite-for-elementor' ) ),
 			'width'  => array( 'label' => esc_html__( 'Border Width', 'wpmozo-addons-lite-for-elementor' ) ),
@@ -160,18 +203,154 @@ $this->add_group_control(
 	)
 );
 $this->add_responsive_control(
-	'portion_border_radius_hover',
+	'bar_border_radius_hover',
 	array(
 		'label'       => esc_html__( 'Border Radius', 'wpmozo-addons-lite-for-elementor' ),
 		'type'        => Controls_Manager::DIMENSIONS,
 		'label_block' => true,
-		'separator'   => 'after',
 		'size_units'  => array( 'px', 'em', '%' ),
 		'selectors'   => array(
-			'{{WRAPPER}} .wpmozo_split_image .wpmozo_split_image_portion:hover' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};transition:all 300ms;',
+			'{{WRAPPER}} .dipl_progress_bar_layout_bar:hover' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};transition:all 300ms;',
 		),
 	)
 );
 $this->end_controls_tab();
 $this->end_controls_tabs();
+$this->end_controls_section();
+$this->start_controls_section(
+	'percentage_text',
+	array(
+		'label' => esc_html__( 'Percentage Text', 'wpmozo-addons-lite-for-elementor' ),
+		'tab'   => Controls_Manager::TAB_STYLE,
+	)
+);
+$this->add_responsive_control( 
+	'percentage_alignment',
+	array( 
+		'label'       => esc_html__( 'Percentage Alignment', 'wpmozo-addons-lite-for-elementor' ),
+		'type'        => Controls_Manager::CHOOSE,
+		'label_block' => true,
+		'separator' => 'after',
+		'options'     => array( 
+			'start'   =>
+				array( 
+					'title' => esc_html__( 'Left', 'wpmozo-addons-lite-for-elementor' ),
+					'icon'  => 'eicon-text-align-left',
+				 ),
+			'center' =>
+				array( 
+					'title' => esc_html__( 'Center', 'wpmozo-addons-lite-for-elementor' ),
+					'icon'  => 'eicon-text-align-center',
+				 ),
+			'end'  =>
+				array( 
+					'title' => esc_html__( 'Right', 'wpmozo-addons-lite-for-elementor' ),
+					'icon'  => 'eicon-text-align-right',
+				 ),
+		 ),
+		'default'     => 'center',
+		'toggle'      => true,
+		'selectors'   => array( '{{WRAPPER}} .dipl_progress_bar_layout_bar .dipl_progress_bar_inner' => 'justify-content: flex-{{VALUE}};' ),
+	)
+);
+$this->start_controls_tabs(
+	'percentage_tabs'
+);
+$this->start_controls_tab(
+	'percentage_normal_tabs',
+	array(
+		'label' => esc_html__( 'Normal', 'wpmozo-addons-lite-for-elementor' ),
+	)
+);
+$this->add_group_control(
+	Group_Control_Typography::get_type(),
+	array(
+		'name'     => 'percentage_typography',
+		'label'    => esc_html__( 'Percentage Typography', 'wpmozo-addons-lite-for-elementor' ),
+		'exclude'  => array( 'font_size' ),
+		'selector' => '{{WRAPPER}} .dipl_progress_bar_percent',
+	)
+);
+$this->add_responsive_control(
+	'percentage_size',
+	array(
+		'label'     => esc_html__( 'Percentage Size', 'wpmozo-addons-lite-for-elementor' ),
+		'type'      => Controls_Manager::SLIDER,
+		'range'     => array(
+			'px' => array(
+				'min'  => 1,
+				'max'  => 100,
+				'step' => 1,
+			),
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .dipl_progress_bar_percent' => 'font-size: {{SIZE}}{{UNIT}}; transition: all 300ms;',
+		),
+	)
+);
+$this->add_responsive_control(
+	'percentage_color',
+	array(
+		'label'     => esc_html__( 'Percentage Color', 'wpmozo-addons-lite-for-elementor' ),
+		'type'      => Controls_Manager::COLOR,
+		'separator' => 'after',
+		'selectors' => array(
+			'{{WRAPPER}} .dipl_progress_bar_percent' => 'color: {{VALUE}}; transition: 300ms;',
+		),
+	)
+);
+$this->end_controls_tab();
+$this->start_controls_tab(
+	'percentage_hover_tabs',
+	array(
+		'label' => esc_html__( 'Hover', 'wpmozo-addons-lite-for-elementor' ),
+	)
+);
+$this->add_group_control(
+	Group_Control_Typography::get_type(),
+	array(
+		'name'     => 'percentage_typography_hover',
+		'label'    => esc_html__( 'Percentage Typography', 'wpmozo-addons-lite-for-elementor' ),
+		'exclude'  => array( 'font_size' ),
+		'selector' => '{{WRAPPER}} .dipl_progress_bar_percent:hover',
+	)
+);
+$this->add_responsive_control(
+	'percentage_size_hover',
+	array(
+		'label'     => esc_html__( 'Percentage Size', 'wpmozo-addons-lite-for-elementor' ),
+		'type'      => Controls_Manager::SLIDER,
+		'range'     => array(
+			'px' => array(
+				'min'  => 1,
+				'max'  => 100,
+				'step' => 1,
+			),
+		),
+		'selectors' => array(
+			'{{WRAPPER}} .dipl_progress_bar_percent:hover' => 'font-size: {{SIZE}}{{UNIT}}; transition: all 300ms;',
+		),
+	)
+);
+$this->add_responsive_control(
+	'percentage_color_hover',
+	array(
+		'label'     => esc_html__( 'Percentage Color', 'wpmozo-addons-lite-for-elementor' ),
+		'type'      => Controls_Manager::COLOR,
+		'separator' => 'after',
+		'selectors' => array(
+			'{{WRAPPER}} .dipl_progress_bar_percent:hover' => 'color: {{VALUE}};',
+		),
+	)
+);
+$this->end_controls_tab();
+$this->end_controls_tabs();
+$this->add_group_control(
+	Group_Control_Text_Shadow::get_type(),
+	array(
+		'name'     => 'percentage_shadow',
+		'label'    => esc_html__( 'Percentage Shadow', 'wpmozo-addons-lite-for-elementor' ),
+		'selector' => '{{WRAPPER}} .dipl_progress_bar_percent',
+	)
+);
 $this->end_controls_section();
--- widgets/progress-bar/progress-bar.php
+++ widgets/progress-bar/progress-bar.php
@@ -141,13 +141,18 @@ if ( ! class_exists( 'WPMOZO_AE_Progress_Bar' ) ) {
 		protected function render() {
 			$settings = $this->get_settings_for_display();
 
-			//$rows    = isset( $settings['image_row']['size'] ) ? $settings['image_row']['size'] : 3;
+			//$rows            = isset( $settings['image_row']['size'] ) ? $settings['image_row']['size'] : 3;
+			$layout            = isset( $settings['layout'] ) ? $settings['layout'] : 'bar';
+			$progress_number   = ( 'yes' === $settings['show_progress_number'] ) ? 'on' : 'off';
+			$show_striped      = ( 'yes' === $settings['show_striped'] ) ? 'striped' : 'no_striped';
+			$bar_direction     = isset( $settings['bar_direction'] ) ? $settings['bar_direction'] : 'horizontal';
+			$no_images_text    = isset( $settings['no_images_text'] ) && ! empty( $settings['no_images_text'] ) ? $settings['no_images_text'] : 'No Images Found!';
 
 			?>
 		
 			<div class="et_pb_module dipl_progress_bar dipl_progress_bar_0">
 				<div class="et_pb_module_inner">
-					<div class="dipl_progress_bar_wrapper dipl_progress_bar_layout_bar" data-show_number="on" data-bar_direction="horizontal">
+					<div class="dipl_progress_bar_wrapper dipl_progress_bar_layout_<?php echo esc_attr($layout); ?> dipl_progress_bar_<?php echo esc_attr($show_striped); ?>" data-show_number="<?php echo esc_attr($progress_number); ?>" data-bar_direction="<?php echo esc_attr($bar_direction); ?>">
 						<div class="dipl_progress_bar_inner" style="width: 72.0842%; height: 100%;">
 							<span class="dipl_progress_bar_percent">72%</span>
 						</div>
