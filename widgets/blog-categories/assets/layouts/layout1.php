b3299fc4ee41c068b44536f40bc1a16a9f9c319c
Sunil Kumar <sunilkumar@Sunils-Laptop.local>
2025-12-26 17:56:50 +0530

    WIP:Blog categories

--- widgets/blog-categories/assets/layouts/layout1.php
+++ widgets/blog-categories/assets/layouts/layout1.php
@@ -6,34 +6,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<div class="dipl_blog_categories">
-	<div class="dipl_blog_categories_wrapper layout1">
-		<div class="dipl_blog_categories_inner">
-			<div id="dipl_blog_category_9" class="dipl_blog_category_item category_diviextended" style="background-image: url(http://elicus-divi.local/wp-content/uploads/2025/01/pexels-tarun-reddy-362129-984802-1.jpg);">
-				<div class="dipl_blog_category_item_inner">
-					<div class="dipl_blog_category_content">
-						<h3 class="dipl_blog_category_name" id="diviextended">diviextended </h3>
-						<div class="dipl_blog_category_count_wrap"><span class="dipl_blog_category_count ">4 Articles</span></div>
-					</div>
-				</div>
-				<a href="http://elicus-divi.local/category/diviextended/" class="dipl_abs_link">diviextended</a></div>
-			<div id="dipl_blog_category_1" class="dipl_blog_category_item category_uncategorized" style="background-image: url(http://elicus-divi.local/wp-content/uploads/2025/01/pexels-nikita-khandelwal-178978-819805-1.jpg);">
-				<div class="dipl_blog_category_item_inner">
-					<div class="dipl_blog_category_content">
-						<h3 class="dipl_blog_category_name" id="uncategorized">Uncategorized </h3>
-						<div class="dipl_blog_category_count_wrap"><span class="dipl_blog_category_count ">1 Articles</span></div>
-					</div>
-				</div>
-				<a href="http://elicus-divi.local/category/uncategorized/" class="dipl_abs_link">Uncategorized</a></div>
-			<div id="dipl_blog_category_8" class="dipl_blog_category_item category_wpmozo" style="background-image: url(http://elicus-divi.local/wp-content/uploads/2025/01/pexels-luftschnitzel-100582-1.jpg);">
-				<div class="dipl_blog_category_item_inner">
-					<div class="dipl_blog_category_content">
-						<h3 class="dipl_blog_category_name" id="wpmozo">wpmozo </h3>
-						<div class="dipl_blog_category_count_wrap"><span class="dipl_blog_category_count ">3 Articles</span></div>
-					</div>
-				</div>
-				<a href="http://elicus-divi.local/category/wpmozo/" class="dipl_abs_link">wpmozo</a></div>
+<div id="dipl_blog_category_<?php echo esc_attr( $cat_id ); ?>" class="dipl_blog_category_item category_<?php echo esc_attr( $cat_slug ); ?>" <?php echo $style_attr; ?> >
+	<div class="dipl_blog_category_item_inner">
+		<div class="dipl_blog_category_content">
+			<h3 class="dipl_blog_category_name" id="<?php echo esc_attr( $cat_slug ); ?>">
+				<?php echo esc_html( $cat_name ); ?>
+			</h3>
+
+			<div class="dipl_blog_category_count_wrap">
+				<span class="dipl_blog_category_count">
+					<?php echo esc_html( $cat_count ); ?> Articles
+				</span>
+			</div>
 		</div>
 	</div>
+
+	<a href="<?php echo esc_url( $cat_link ); ?>" class="dipl_abs_link">
+		<?php echo esc_html( $cat_name ); ?>
+	</a>
 </div>
 <?php
